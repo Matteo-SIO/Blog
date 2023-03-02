5350376c7f66d1cfd6fc4bccaae6231a6a939387
users <mattbacq2004@mail.com>
2023-03-02 11:12:56 +0100

    Fixed path

--- api/mainloader.php
+++ api/mainloader.php
@@ -5,7 +5,7 @@ require __DIR__ .'/../vendor/autoload.php';
 require_once __DIR__ .'/../generated-conf/config.php';
 
 // static variables
-require ('../config/global.php');
+require __DIR__ .'../../config/global.php';
 
 // dynamically load all API files
 $files = glob( __DIR__ .'/*.php');
