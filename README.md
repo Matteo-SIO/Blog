34cb52b833f33b09e19f2b08404382c5496f101c
users <mattbacq2004@mail.com>
2023-03-02 09:29:55 +0100

    Enhanced README.md

--- README.md
+++ README.md
@@ -14,13 +14,29 @@
 * execute ``./vendor/bin/propel sql:build`` to build the sql files
 * execute ``./vendor/bin/propel sql:insert`` to insert the sql files into the database
 
-# Usage:
-* The root page is in ``/pages/``
-* This repo is public
+## Create admin user:
+* Modify ``adminInstall.php`` with your desired username and password
+* execute ``php ./adminInstall.php``
+
+# Content of the project:
+* ``/pages/index.php``: root page (listing recent posts)
+* ``/pages/admin.php``: admin page (can manage roles and users)
+## Authentication:
+* ``/pages/login/register.php``: create new account and connect to it
+* ``/pages/login/login.php``: connect to existent account
+* ``/pages/login/logout.php``: destroy session
+## Publishing:
+* ``/pages/publisher/publisher.php``: create new post
+## Reading:
+* ``/pages/view/view.php``: read a post (with comments, can moderate if moderator)
 
 # Maintenance:
 * on change of ``schema.xml``, do 'database' steps above
 * Read propel2 documentation for more information about ORM
+
+# Usage:
+* The root page is in ``/pages/``
+* This repo is public
 
 # Credits:
 * [Propel](https://github.com/propelorm/Propel2)
---
+++ adminInstall.php
@@ -0,0 +1,45 @@
+<?php
+
+use Blog\Role;
+use Blog\User;
+use Blog\UserQuery;
+
+require '../api/mainloader.php';
+
+/* MODIFY HERE ONLY */
+const ADMIN_EMAIL = 'admin@company.com';
+const ADMIN_DISPLAY = 'Admin';
+const ADMIN_PASSWORD = 'admin';
+
+const ROLE_DISPLAY_ADMIN = 'Admin';
+const ROLE_DISPLAY_MODERATOR = 'Modérateur';
+const ROLE_DISPLAY_WRITER = 'Rédacteur';
+/* MODIFY HERE ONLY */
+
+$adminRole = new Role();
+$adminRole->setDisplay(ROLE_DISPLAY_ADMIN);
+$adminRole->setCanAdministrate(true);
+$adminRole->setCanmoderate(true);
+$adminRole->setCanWrite(true);
+$adminRole->save();
+
+$moderatorRole = new Role();
+$moderatorRole->setDisplay(ROLE_DISPLAY_MODERATOR);
+$moderatorRole->setCanAdministrate(false);
+$moderatorRole->setCanmoderate(true);
+$moderatorRole->setCanWrite(true);
+$moderatorRole->save();
+
+$writerRole = new Role();
+$writerRole->setDisplay(ROLE_DISPLAY_WRITER);
+$writerRole->setCanAdministrate(false);
+$writerRole->setCanmoderate(false);
+$writerRole->setCanWrite(true);
+$writerRole->save();
+
+$user = new User();
+$user->setEmail(ADMIN_EMAIL);
+$user->setPassword(password_hash(ADMIN_PASSWORD, PASSWORD_DEFAULT));
+$user->setDisplay(ADMIN_DISPLAY);
+$user->setRolesObj($adminRole);
+$user->save();
